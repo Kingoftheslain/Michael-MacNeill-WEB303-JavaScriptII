b068246ad843adeb3113ca4bb817ee7bdc1432c7
Michael MacNeill <98714598+Kingoftheslain@users.noreply.github.com>
2022-10-26 18:52:28 -0400

    Adding Base Files to Repo
    I am adding the files to the repository before I start adding code to the script page, I have however added some code to the HTML page, Including an additional Img to the assignment to give my assignment a bit more originality.

---
+++ WEB303As05/script.js
@@ -0,0 +1,13 @@
+/*
+    Assignment 05
+    Author Name: Michael MacNeill
+    Student #: 0237866
+    Date: Oct. 26th, 22
+*/
+
+$(document).ready(function () {
+    // your code here
+    
+});
+
+
